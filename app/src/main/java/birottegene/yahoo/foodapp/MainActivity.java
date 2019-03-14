d536659c0bb3eee85390f34723639a6b70761f9b
GeneBirotte <birottegene@yahoo.com>
2019-03-14 12:27:37 -0400

    hello world

---
+++ app/src/main/java/birottegene/yahoo/foodapp/MainActivity.java
@@ -0,0 +1,33 @@
+package birottegene.yahoo.foodapp;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+   ListView myQuickListView;
+   String [] items;
+   String [] prices;
+   String [] descriptions;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        myQuickListView = (ListView) findViewById(R.id.myQiuckListView);
+        items = res.getStringArray(R.array.items);
+        prices = res.getStringArray(R.array.prices);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
+        myQuickListView.setAdapter(itemAdapter);
+
+
+    }
+}
